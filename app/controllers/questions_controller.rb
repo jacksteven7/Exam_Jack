eaf501ade0b48fd11bc32ef13af0f6ec291745ae
jacksteven7 <jacksteven7@gmail.com>
2015-03-23 17:16:29 -0500

    Adding Answers to Exam

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,11 @@
 class QuestionsController < ApplicationController
-  
+  def create
+  	@exam = Exam.find(params.require(:exam_id))
+  	@question = @exam.questions.create(params.require(:question).permit(:title,:type))
+  	redirect_to @exam
+  end
+  def show
+  	@exam = Exam.find(params.require(:exam_id))
+  	@question = @exam.questions.create(params.require(:question).permit(:title,:type))
+  end
 end
