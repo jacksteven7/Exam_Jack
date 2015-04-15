9f9e6d8fa9abf8321325b9d3b0d5ea18ee8619d6
jacksteven7 <jacksteven7@gmail.com>
2015-04-15 00:22:11 -0500

    Managing the Answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,12 +1,19 @@
 class AnswersController < ApplicationController
   def create
-  	@exam = Exam.find(params.require(:exam_id))
-  	@question = @exam.questions.find_by(id: params.require(:question_id))
-	@answer = @question.answers.create(params.require(:answer).permit(:title))
-	redirect_to exam_question_path(@exam,@question)
+    @exam = Exam.find(params.require(:exam_id))
+    @question = @exam.questions.find_by(id: params.require(:question_id))
+    @answer = @question.answers.create(params.require(:answer).permit(:title,:correct))
+    redirect_to exam_question_path(@exam,@question)
   end
   def show
-  	@exam = Exam.find(params.require(:exam_id))
-  	@question = @exam.questions.find_by(id: params.require(:id))
+    @exam = Exam.find(params.require(:exam_id))
+    @question = @exam.questions.find_by(id: params.require(:id))
+  end
+  def destroy
+    @exam = Exam.find(params.require(:exam_id))
+    @question = @exam.questions.find_by(id: params.require(:question_id))
+    @answer = @question.answers.find_by(id: params.require(:id))
+    @answer.destroy
+    redirect_to exam_question_path(@exam,@question)
   end
 end
